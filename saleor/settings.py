316675da106e1f3bde12195e37b7ada823412fd1
Zatin009 <40449637+yinmazatinKIT@users.noreply.github.com>
2019-08-26 23:46:49 +0700

    Alter USE_JSON_CONTEN in setting.py to True

--- saleor/settings.py
+++ saleor/settings.py
@@ -687,4 +687,4 @@ PLUGINS = os.environ.get("PLUGINS", [])
 # Whether DraftJS should be used be used instead of HTML
 # True to use DraftJS (JSON based), for the 2.0 dashboard
 # False to use the old editor from dashboard 1.0
-USE_JSON_CONTENT = get_bool_from_env("USE_JSON_CONTENT", False)
+USE_JSON_CONTENT = get_bool_from_env("USE_JSON_CONTENT", True)
