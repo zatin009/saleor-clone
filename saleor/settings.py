0d6cb4053af80213dd27775a64cf724e1bd77e7c
Zatin009 <40449637+yinmazatinKIT@users.noreply.github.com>
2019-09-06 15:27:47 +0700

    make eseller better

--- saleor/settings.py
+++ saleor/settings.py
@@ -199,7 +199,8 @@ TEMPLATES = [
 ]
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY ="123454321"
 
 MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
